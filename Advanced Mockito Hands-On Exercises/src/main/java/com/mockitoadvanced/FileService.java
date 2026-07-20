17e01f99004ad531170124d702b859dce921609b
Megamala19 <mightymegu@gmail.com>
2026-07-20 14:46:01 +0530

    Create FileService.java

---
+++ Advanced Mockito Hands-On Exercises/src/main/java/com/mockitoadvanced/FileService.java
@@ -0,0 +1,22 @@
+package com.example.mockitoadvanced;
+
+/**
+ * Service under test for Exercise 3 (mocking file I/O).
+ */
+public class FileService {
+
+    private final FileReader fileReader;
+    private final FileWriter fileWriter;
+
+    public FileService(FileReader fileReader, FileWriter fileWriter) {
+        this.fileReader = fileReader;
+        this.fileWriter = fileWriter;
+    }
+
+    public String processFile() {
+        String content = fileReader.read();
+        String processed = "Processed " + content;
+        fileWriter.write(processed);
+        return processed;
+    }
+}
