60bff1f3d225d1b18df581e20a69b0812520b357
Megamala19 <mightymegu@gmail.com>
2026-07-20 15:35:21 +0530

    Delete Mocking Dependencies in Spring Tests using Mockito/src/main/java directory

--- Mocking Dependencies in Spring Tests using Mockito/src/main/java/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mockitodeps.repository;
-
-import com.example.mockitodeps.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
