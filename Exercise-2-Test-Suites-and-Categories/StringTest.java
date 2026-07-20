6d130402ab4a9e1ec23788469fcaef578e873cfd
Megamala19 <mightymegu@gmail.com>
2026-07-20 14:10:39 +0530

    Create StringTest.java

---
+++ Exercise-2-Test-Suites-and-Categories/StringTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class StringTest {
+    @Test
+    void testStringLength() {
+        assertEquals(5, "Hello".length());
+    }
+}
