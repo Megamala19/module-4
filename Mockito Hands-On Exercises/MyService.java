21dc2b364db89b11f13fd40b719aac5bb7fbf7bf
Megamala19 <mightymegu@gmail.com>
2026-07-20 14:33:18 +0530

    Create MyService.java

---
+++ Mockito Hands-On Exercises/MyService.java
@@ -0,0 +1,42 @@
+package com.example.mockito;
+
+/**
+ * Service under test. Depends on ExternalApi, which we mock in tests.
+ */
+public class MyService {
+
+    private final ExternalApi externalApi;
+
+    public MyService(ExternalApi externalApi) {
+        this.externalApi = externalApi;
+    }
+
+    public String fetchData() {
+        return externalApi.getData();
+    }
+
+    public String fetchDataWithArg(String arg) {
+        externalApi.performAction(arg);
+        return "processed:" + arg;
+    }
+
+    public String fetchTwice() {
+        String first = externalApi.getData();
+        String second = externalApi.getData();
+        return first + "|" + second;
+    }
+
+    public void doOrderedWork() {
+        externalApi.performAction("first");
+        externalApi.performAction("second");
+    }
+
+    public String safeRiskyCall() {
+        try {
+            externalApi.riskyAction();
+            return "ok";
+        } catch (RuntimeException e) {
+            return "handled:" + e.getMessage();
+        }
+    }
+}
